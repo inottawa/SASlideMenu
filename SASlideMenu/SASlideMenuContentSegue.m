7f0e05cdc399b69375ecbce0718281895f7d6c34
Ryan Spears <ryan@spears.ca>
2013-10-22 13:06:28 -0400

    unique instance of the menu button for tabbar

---
+++ SASlideMenu/SASlideMenuContentSegue.m
@@ -0,0 +1,116 @@
+//
+//  SASlideContentSegue.m
+//  SASlideMenu
+//
+//  Created by Stefano Antonelli on 1/17/13.
+//  Edited by Ryan Spears on 6/18/13.
+//  Copyright (c) 2013 Stefano Antonelli. All rights reserved.
+//
+#import <QuartzCore/QuartzCore.h>
+#import "SASlideMenuContentSegue.h"
+#import "SASlideMenuRootViewController.h"
+#import "SASlideMenuViewController.h"
+
+@implementation SASlideMenuContentSegue
+
+-(void) perform{
+ 
+    SASlideMenuViewController* source = self.sourceViewController;
+    SASlideMenuRootViewController* rootController = source.rootController;
+    UINavigationController* destination = self.destinationViewController;
+
+    UIButton* menuButton;
+    
+    if ([destination isKindOfClass:[UINavigationController class]]) {
+        UINavigationItem* navigationItem = destination.navigationBar.topItem;
+        [menuButton addTarget:rootController action:@selector(doSlideToSide) forControlEvents: UIControlEventTouchUpInside];
+        menuButton = [[UIButton alloc] init];
+        navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:menuButton];
+        if ([rootController.leftMenu.slideMenuDataSource respondsToSelector:@selector(configureMenuButton:)])
+            [rootController.leftMenu.slideMenuDataSource configureMenuButton:menuButton];
+    } else if ([destination isKindOfClass:[UISplitViewController class]]) {
+        UISplitViewController *sv = (UISplitViewController *)destination;
+        id viewController = [sv.viewControllers objectAtIndex:0];
+        if([viewController isMemberOfClass:[UITabBarController class]]) {
+            UITabBarController *tabBar = (UITabBarController*)viewController;
+            for (id idController in tabBar.viewControllers) {
+                if([idController isMemberOfClass:[UINavigationController class]]) {
+                    UINavigationController *nvController = (UINavigationController*)idController;
+                    if(nvController) {
+                        UINavigationItem* navigationItem = nvController.navigationBar.topItem;
+                        menuButton = [[UIButton alloc] init];
+                        [menuButton addTarget:rootController action:@selector(doSlideToSide) forControlEvents:UIControlEventTouchUpInside];
+                        navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:menuButton];
+                        if ([rootController.leftMenu.slideMenuDataSource respondsToSelector:@selector(configureSplitMenuButton:)])
+                            [rootController.leftMenu.slideMenuDataSource configureSplitMenuButton:menuButton];
+                    }
+                }
+            }
+        } else {
+            UINavigationController *navC = [sv.viewControllers objectAtIndex:0];
+            UINavigationItem* navigationItem = navC.navigationBar.topItem;
+            menuButton = [[UIButton alloc] init];            
+            [menuButton addTarget:rootController action:@selector(doSlideToSide) forControlEvents:UIControlEventTouchUpInside];
+            navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:menuButton];
+            if ([rootController.leftMenu.slideMenuDataSource respondsToSelector:@selector(configureSplitMenuButton:)])
+                [rootController.leftMenu.slideMenuDataSource configureSplitMenuButton:menuButton];
+        }
+    }
+    
+    Boolean hasRightMenu = NO;
+    rootController.isRightMenuEnabled = NO;
+    NSIndexPath* selectedIndexPath = [rootController.leftMenu.tableView indexPathForSelectedRow];
+
+    if ([rootController.leftMenu.slideMenuDataSource respondsToSelector:@selector(hasRightMenuForIndexPath:)]) {
+        hasRightMenu = [rootController.leftMenu.slideMenuDataSource hasRightMenuForIndexPath:selectedIndexPath];
+    }
+    if (hasRightMenu) {
+        rootController.isRightMenuEnabled = YES;
+        if ([rootController.leftMenu.slideMenuDataSource respondsToSelector:@selector(configureRightMenuButton:)]) {
+            UIButton* rightMenuButton = [[UIButton alloc] init];
+            [rootController.leftMenu.slideMenuDataSource configureRightMenuButton:rightMenuButton];
+            [rightMenuButton addTarget:rootController action:@selector(rightMenuAction) forControlEvents:UIControlEventTouchUpInside];
+            
+            UINavigationItem* rightNavigationItem = destination.navigationBar.topItem;
+            rightNavigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:rightMenuButton];
+        }
+    }
+
+    if([rootController.leftMenu.slideMenuDataSource respondsToSelector:@selector(configureSlideLayer:)]) {
+        [rootController.leftMenu.slideMenuDataSource configureSlideLayer:[destination.view layer]];        
+    }else{
+        CALayer* layer = destination.view.layer;
+        layer.shadowColor = [UIColor blackColor].CGColor;
+        layer.shadowOpacity = 0.3;
+        layer.shadowOffset = CGSizeMake(-15, 0);
+        layer.shadowRadius = 10;
+        layer.masksToBounds = NO;
+        layer.shadowPath =[UIBezierPath bezierPathWithRect:layer.bounds].CGPath;
+    }
+    
+    [rootController switchToContentViewController:destination completion:^{
+        if ([rootController.leftMenu.slideMenuDataSource respondsToSelector:@selector(segueIdForIndexPath:)]) {
+            [rootController addContentViewController:destination withIndexPath:selectedIndexPath];
+        }
+        
+        //We do that after switchToContentViewController.
+        //because we need destination.parentViewController to be initialized.
+        if (hasRightMenu) {
+            [destination performSegueWithIdentifier:@"rightMenu" sender:rootController];
+        }
+        
+        Boolean disablePanGesture= NO;
+        if ([rootController.leftMenu.slideMenuDataSource respondsToSelector:@selector(disablePanGestureForIndexPath:)]) {
+            disablePanGesture = [rootController.leftMenu.slideMenuDataSource disablePanGestureForIndexPath:selectedIndexPath];
+        }
+        if (!disablePanGesture) {
+            UIPanGestureRecognizer* panGesture= [[UIPanGestureRecognizer alloc] initWithTarget:rootController action:@selector(panItem:)];
+            [panGesture setMaximumNumberOfTouches:2];
+            [panGesture setDelegate:source];
+            [destination.view addGestureRecognizer:panGesture];
+        }        
+    }];
+
+}
+
+@end
